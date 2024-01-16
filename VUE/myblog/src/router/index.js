6c4ef38df726202e3c1880b609fc64f34af6c5ba
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 22:31:48 +0800

    路由修改

---
+++ VUE/myblog/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter,createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes:[{
+        path:"/login",
+        component:()=>import("@/views/Login/LoginPage.vue")
+    },{
+        path:"/home",
+        component:()=>import("@/views/Home/HomePage.vue")
+    },{
+        path:"/article",
+        component:()=>import("@/views/Detail/DetailPage.vue")
+    },{
+        path:"/",
+        redirect:"/home"
+    },{
+        path:"/:pathMatch(.*)",
+        component:()=>import("@/views/NotFound.vue")
+    }]
+})
+
+export default router
