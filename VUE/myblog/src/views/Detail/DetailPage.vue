47f830fba933520cb05f77f108f1505149459051
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-17 13:55:14 +0800

    文章详情

--- VUE/myblog/src/views/Detail/DetailPage.vue
+++ VUE/myblog/src/views/Detail/DetailPage.vue
@@ -1,15 +1,26 @@
 <template>
-
     <div class="detail">
-        <h2>article</h2>
+      <h2>article</h2>
+  
+      <h1>标题：{{ articleData.title }}</h1>
+  
+      <div v-html="articleData.content">
+  
+      </div>
+  
     </div>
-   
-   </template>
-   
-   <script setup> 
-   
-   
-   </script>
-   <style scoped>
-   
-   </style>
+  </template>
+  
+  <script setup>
+  import useArticles from "@/stores/useArticle";
+  import { toRef } from "vue";
+  import { useRoute } from "vue-router";
+  
+  
+  const route = useRoute()
+  const ArticleStore = useArticles()
+  
+  const articleData = toRef(ArticleStore.getArticleById(route.params.articleId))
+  </script>
+  
+  <style></style>
